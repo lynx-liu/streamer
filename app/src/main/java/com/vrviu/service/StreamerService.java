9d6be68260991a7c9928715692a4ac916273e386
lynx.liu <lynx.liu@vrviu.com>
2022-11-07 11:00:57 +0800

    refactor: remove the connection to the control channel by reading the configuration file parameters when the APP starts##DEV-29597

--- app/src/main/java/com/vrviu/service/StreamerService.java
+++ app/src/main/java/com/vrviu/service/StreamerService.java
@@ -2,7 +2,6 @@ package com.vrviu.service;
 
 import android.accessibilityservice.AccessibilityService;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.graphics.Rect;
@@ -26,19 +25,11 @@ import com.vrviu.utils.SurfaceControl;
 import com.vrviu.utils.SystemUtils;
 
 public class StreamerService extends AccessibilityService {
-    private static final String lsIpField = "lsIp";
-    private static final String startStreamingField = "startStreaming";
-    private static final String lsControlPortField = "lsControlPort";
-    private static final String isGameModeField = "isGameMode";
-    private static final String downloadDirField = "downloadDir";
-    private static final String packageNameField = "packageName";
-    private static final String defaultIP = "10.0.2.2";
     private static final int NOT_IN_GAME = 9998;
     private static final int MSG_UPDATE_VIEW = 0x01;
     private static final int MAX_DELAY = 500;
     private static int delayMillis = MAX_DELAY;
 
-    private SharedPreferences preferences = null;
     private ControlTcpClient controlTcpClient = null;
 
     private static int color = 0;
@@ -61,20 +52,6 @@ public class StreamerService extends AccessibilityService {
         display.getRealSize(screenSize);
         displayManager.registerDisplayListener(displayListener,null);
         Log.d("llx","width:"+screenSize.x+", height:"+screenSize.y+", orientation:"+display.getRotation());
-
-        Context deviceContext = createDeviceProtectedStorageContext();
-        preferences = deviceContext.getSharedPreferences(getPackageName(),Context.MODE_PRIVATE);
-        if(preferences.getBoolean(startStreamingField,false)) {
-            String ip = preferences.getString(lsIpField, defaultIP);
-            int port = preferences.getInt(lsControlPortField, 5000);
-            boolean isGameMode = preferences.getBoolean(isGameModeField, true);
-            String downloadDir = preferences.getString(downloadDirField, null);
-            String packageName = preferences.getString(packageNameField, null);
-
-            controlTcpClient = new ControlTcpClient(getApplicationContext(), ip, port, isGameMode, downloadDir, packageName, null);
-            controlTcpClient.start();
-            controlTcpClient.setDisplayRotation(screenSize);
-        }
 
         videoTcpServer.start();
         createFloatWindow();
@@ -175,16 +152,6 @@ public class StreamerService extends AccessibilityService {
         @Override
         public boolean startStreaming(String flowId, String lsIp, boolean tcp, int lsVideoPort, int lsAudioPort, int lsControlPort, boolean h264, String videoCodecProfile, int idrPeriod, int maxFps, int minFps, int width, int height, int bitrate, int orientationType, int enableSEI, int rateControlMode, int gameMode, String packageName, String downloadDir) {
             boolean isGameMode = gameMode!=NOT_IN_GAME;
-            if(preferences!=null) {
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putBoolean(startStreamingField,true);
-                editor.putString(lsIpField,lsIp);
-                editor.putInt(lsControlPortField,lsControlPort);
-                editor.putBoolean(isGameModeField,isGameMode);
-                editor.putString(downloadDirField,downloadDir);
-                editor.putString(packageNameField,packageName);
-                editor.apply();
-            }
 
             if(controlTcpClient!=null) controlTcpClient.interrupt();
             controlTcpClient = new ControlTcpClient(getApplicationContext(),lsIp,lsControlPort,isGameMode,downloadDir,packageName,null);
@@ -224,12 +191,6 @@ public class StreamerService extends AccessibilityService {
 
         @Override
         public void stopStreaming(boolean stopVideo, boolean stopAudio, boolean stopControl) {
-            if(preferences!=null) {
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putBoolean(startStreamingField,false);
-                editor.apply();
-            }
-
             if(controlTcpClient!=null)
                 controlTcpClient.interrupt();
             mhandler.removeMessages(MSG_UPDATE_VIEW);
