4578a6034200729412f7c6b9ad806e57463f2a28
lynx_icloudsky <lynx.liu@icloudsky.com>
2023-11-17 16:01:04 +0800

    fix: set KEY_INTRA_REFRESH_PERIOD to maxFps*30 ##DEV-35650

--- app/src/main/cpp/VideoEncoder.cpp
+++ app/src/main/cpp/VideoEncoder.cpp
@@ -121,7 +121,7 @@ ANativeWindow* VideoEncoder::createEncoder(AMediaMuxer *muxer) {
     AMediaFormat_setInt32(videoFormat, AMEDIAFORMAT_KEY_BIT_RATE,videoParam.bitrate);
     AMediaFormat_setInt32(videoFormat, AMEDIAFORMAT_KEY_FRAME_RATE, videoParam.maxFps);
     AMediaFormat_setInt32(videoFormat, AMEDIAFORMAT_KEY_I_FRAME_INTERVAL, videoParam.frameInterval);
-    AMediaFormat_setInt32(videoFormat, AMEDIAFORMAT_KEY_INTRA_REFRESH_PERIOD, videoParam.maxFps);
+    AMediaFormat_setInt32(videoFormat, AMEDIAFORMAT_KEY_INTRA_REFRESH_PERIOD, videoParam.maxFps*30);
     AMediaFormat_setInt64(videoFormat, AMEDIAFORMAT_KEY_REPEAT_PREVIOUS_FRAME_AFTER, (long) (1.5*(1000000L/videoParam.maxFps+1))); // µs
     AMediaFormat_setInt32(videoFormat, AMEDIAFORMAT_KEY_COLOR_FORMAT, COLOR_FormatSurface);
     AMediaFormat_setFloat(videoFormat, AMEDIAFORMAT_KEY_MAX_FPS_TO_ENCODER, videoParam.maxFps);
